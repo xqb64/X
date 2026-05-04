fb2629f492170d28c28a69147ce85236e0948083
xqb64 <x@xqb.ch>
2026-05-04 11:44:06 +0200

    feat: preliminary parsing

--- src/lang.c
+++ src/lang.c
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <stdarg.h>
+#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -95,7 +97,9 @@ enum TokenKind {
   TOKEN_RETURN,
   TOKEN_NUMBER,
   TOKEN_PLUS,
+  TOKEN_MINUS,
   TOKEN_STAR,
+  TOKEN_SLASH,
   TOKEN_SEMICOLON,
   TOKEN_RBRACE,
   TOKEN_ERROR,
@@ -173,8 +177,7 @@ int lookahead(struct Tokenizer *tokenizer, int n, char *target)
 
 struct Token mktoken(struct Tokenizer *tokenizer, enum TokenKind kind, int len)
 {
-  struct Token token = {
-      .kind = kind, .start = tokenizer->src - len, .len = len};
+  struct Token token = {.kind = kind, .start = tokenizer->src, .len = len};
   tokenizer->src += len;
   return token;
 }
@@ -360,6 +363,583 @@ void print_tokens(VecToken tokens)
   }
 }
 
+enum LiteralKind {
+  LITERAL_NUM,
+  LITERAL_STR,
+};
+
+struct Literal {
+  enum LiteralKind kind;
+  union {
+    char *str;
+    int num;
+  } as;
+};
+
+enum ExprKind {
+  EXPR_LITERAL,
+  EXPR_BINARY,
+};
+
+enum ExprBinKind {
+  EXPR_BIN_ADD,
+  EXPR_BIN_SUB,
+  EXPR_BIN_MUL,
+  EXPR_BIN_DIV,
+};
+
+struct ExprBin {
+  enum ExprBinKind kind;
+  struct Expr *lhs;
+  struct Expr *rhs;
+};
+
+struct Expr {
+  enum ExprKind kind;
+  union {
+    struct Literal literal;
+    struct ExprBin binary;
+  } as;
+};
+
+typedef Vector(struct Stmt) VecStmt;
+
+struct StmtRet {
+  struct Expr val;
+};
+
+struct StmtBlock {
+  VecStmt stmts;
+};
+
+enum StmtKind {
+  STMT_FN,
+  STMT_BLOCK,
+  STMT_RET,
+};
+
+struct StmtFn {
+  char *name;
+  char **params;
+  VecStmt body;
+};
+
+struct Stmt {
+  enum StmtKind kind;
+  union {
+    struct StmtRet ret;
+    struct StmtFn fn;
+    struct StmtBlock block;
+  } as;
+};
+
+struct AST {
+  VecStmt stmts;
+};
+
+struct ParseResult {
+  bool is_ok;
+  char *msg;
+  struct AST *ast;
+};
+
+struct Parser {
+  struct Token *curr;
+  struct Token *prev;
+  VecToken *tokens;
+  int idx;
+};
+
+struct Token *next_token(struct Parser *parser)
+{
+  if (parser->idx < parser->tokens->len) {
+    return &parser->tokens->data[parser->idx++];
+  }
+  return NULL;
+}
+
+struct Token *advance_parser(struct Parser *parser)
+{
+  parser->prev = parser->curr;
+  parser->curr = next_token(parser);
+
+  return parser->prev;
+}
+
+void init_parser(struct Parser *parser, VecToken *tokens)
+{
+  parser->idx = 0;
+  parser->tokens = tokens;
+  parser->prev = NULL;
+  parser->curr = NULL;
+
+  advance_parser(parser);
+}
+
+struct ParseFnResult {
+  bool is_ok;
+  char *msg;
+  union {
+    struct Expr expr;
+    struct Stmt stmt;
+  } as;
+};
+
+struct Token *consume(struct Parser *parser, enum TokenKind kind)
+{
+  if (parser->curr && parser->curr->kind == kind) {
+    return advance_parser(parser);
+  }
+  printf("Encountered wrong token:");
+  print_token(parser->prev);
+  printf("\n");
+  return NULL;
+}
+
+struct ParseFnResult parse_stmt(struct Parser *parser);
+
+bool check(struct Parser *parser, enum TokenKind kind)
+{
+  return parser->curr->kind == kind;
+}
+
+struct ParseFnResult block(struct Parser *parser)
+{
+  VecStmt stmts = {0};
+  struct ParseFnResult result;
+
+  result.is_ok = true;
+  result.msg = NULL;
+
+  while (!check(parser, TOKEN_RBRACE)) {
+    struct ParseFnResult r;
+    struct Stmt s;
+
+    r = parse_stmt(parser);
+    if (!r.is_ok) {
+      return r;
+    }
+
+    vec_insert(&stmts, r.as.stmt);
+  }
+
+  struct Stmt block_stmt;
+  block_stmt.kind = STMT_BLOCK;
+  block_stmt.as.block.stmts = stmts;
+
+  result.as.stmt = block_stmt;
+
+  return result;
+}
+
+char *own_string_n(const char *string, int n)
+{
+  char *s = malloc(strlen(string) + 1);
+  snprintf(s, n + 1, "%s", string);
+  return s;
+}
+
+struct ParseFnResult parse_fn_stmt(struct Parser *parser)
+{
+  struct ParseFnResult result;
+  struct Token *token_fn, *token_id, *token_lparen, *token_void, *token_rparen,
+      *token_lbrace, *token_rbrace;
+
+  result.is_ok = true;
+  result.msg = NULL;
+
+  token_fn = consume(parser, TOKEN_FN);
+  if (!token_fn) {
+    return (struct ParseFnResult){
+        .is_ok = false, .as.stmt = {0}, .msg = "Expected token 'fn'"};
+  }
+
+  token_id = consume(parser, TOKEN_IDENTIFIER);
+  if (!token_id) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Expected token 'id' after 'fn'"};
+  }
+
+  token_lparen = consume(parser, TOKEN_LPAREN);
+  if (!token_lparen) {
+    return (struct ParseFnResult){
+        .is_ok = false, .as.stmt = {0}, .msg = "Expected token '(' after 'id'"};
+  }
+
+  token_void = consume(parser, TOKEN_VOID);
+  if (!token_void) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Expected token 'void' after '('"};
+  }
+
+  token_rparen = consume(parser, TOKEN_RPAREN);
+  if (!token_rparen) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Expected token ')' after 'void'"};
+  }
+
+  token_lbrace = consume(parser, TOKEN_LBRACE);
+  if (!token_lbrace) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Expected token 'void' after '('"};
+  }
+
+  struct ParseFnResult block_result = block(parser);
+  if (!block_result.is_ok) {
+    return (struct ParseFnResult){
+        .is_ok = false, .as.stmt = {0}, .msg = "Expected block after '{'"};
+  }
+
+  struct Stmt body = block_result.as.stmt;
+
+  token_rbrace = consume(parser, TOKEN_RBRACE);
+  if (!token_rbrace) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Expected token '}' after fn body"};
+  }
+
+  struct StmtFn stmt_fn;
+  stmt_fn.body = body.as.block.stmts;
+  stmt_fn.name = own_string_n(token_id->start, token_id->len);
+  stmt_fn.params = NULL;
+
+  struct Stmt stmt;
+  stmt.kind = STMT_FN;
+  stmt.as.fn = stmt_fn;
+
+  result.as.stmt = stmt;
+
+  return result;
+}
+
+static bool match(struct Parser *parser, int size, ...)
+{
+  va_list ap;
+  va_start(ap, size);
+  for (int i = 0; i < size; i++) {
+    enum TokenKind kind = va_arg(ap, enum TokenKind);
+    if (check(parser, kind)) {
+      advance_parser(parser);
+      va_end(ap);
+      return true;
+    }
+  }
+  va_end(ap);
+  return false;
+}
+
+#define ALLOC(obj) (memcpy(malloc(sizeof((obj))), &(obj), sizeof(obj)))
+
+struct ParseFnResult parse_literal(struct Parser *parser)
+{
+  struct ParseFnResult res;
+  struct Literal literal;
+
+  res.is_ok = true;
+  res.msg = NULL;
+
+  if (!consume(parser, TOKEN_NUMBER)) {
+    res.is_ok = false;
+    res.msg = "Expected number";
+    return res;
+  }
+
+  literal.kind = LITERAL_NUM;
+  literal.as.num = strtol(parser->prev->start, NULL, 10);
+
+  res.as.expr = (struct Expr){.kind = EXPR_LITERAL, .as.literal = literal};
+
+  return res;
+}
+
+struct ParseFnResult factor(struct Parser *parser)
+{
+  struct ParseFnResult left_res, right_res;
+  struct Expr left, right;
+
+  left_res = parse_literal(parser);
+  if (!left_res.is_ok) {
+    return left_res;
+  }
+
+  left = left_res.as.expr;
+  while (match(parser, 2, TOKEN_STAR, TOKEN_SLASH)) {
+    char *op = own_string_n(parser->prev->start, 1);
+
+    right_res = parse_literal(parser);
+    if (!right_res.is_ok) {
+      return right_res;
+    }
+
+    right = right_res.as.expr;
+
+    struct ExprBin binexpr;
+
+    switch (*op) {
+      case '*':
+        binexpr.kind = EXPR_BIN_MUL;
+        break;
+      case '/':
+        binexpr.kind = EXPR_BIN_DIV;
+        break;
+      default:
+        printf("op is %s\n", op);
+        assert(0);
+    }
+
+    binexpr.lhs = ALLOC(left);
+    binexpr.rhs = ALLOC(right);
+
+    left = (struct Expr){.kind = EXPR_BINARY, .as.binary = binexpr};
+
+    left_res.as.expr = left;
+  }
+
+  return left_res;
+}
+
+struct ParseFnResult term(struct Parser *parser)
+{
+  struct ParseFnResult left_res, right_res;
+  struct Expr left, right;
+
+  left_res = factor(parser);
+  if (!left_res.is_ok) {
+    return left_res;
+  }
+
+  left = left_res.as.expr;
+  while (match(parser, 2, TOKEN_PLUS, TOKEN_MINUS)) {
+    char *op = own_string_n(parser->prev->start, 1);
+
+    right_res = factor(parser);
+    if (!right_res.is_ok) {
+      return right_res;
+    }
+
+    right = right_res.as.expr;
+
+    struct ExprBin binexpr;
+
+    switch (*op) {
+      case '+':
+        binexpr.kind = EXPR_BIN_ADD;
+        break;
+      case '-':
+        binexpr.kind = EXPR_BIN_SUB;
+        break;
+      default:
+        assert(0);
+    }
+
+    binexpr.lhs = ALLOC(left);
+    binexpr.rhs = ALLOC(right);
+
+    left = (struct Expr){.kind = EXPR_BINARY, .as.binary = binexpr};
+
+    left_res.as.expr = left;
+  }
+
+  return left_res;
+}
+
+struct ParseFnResult parse_expr(struct Parser *parser)
+{
+  struct ParseFnResult res;
+  struct Expr expr;
+
+  res.is_ok = true;
+  res.msg = NULL;
+
+  res = term(parser);
+  if (!res.is_ok) {
+    return res;
+  }
+
+  expr = res.as.expr;
+
+  return (struct ParseFnResult){.is_ok = true, .msg = NULL, .as.expr = expr};
+}
+
+struct ParseFnResult parse_ret_stmt(struct Parser *parser)
+{
+  struct ParseFnResult result;
+  struct Token *token_ret, *token_semicolon;
+
+  result.is_ok = true;
+  result.msg = NULL;
+
+  token_ret = consume(parser, TOKEN_RETURN);
+  if (!token_ret) {
+    return (struct ParseFnResult){
+        .is_ok = false, .as.stmt = {0}, .msg = "Expected token 'return'"};
+  }
+
+  struct ParseFnResult expr_res = parse_expr(parser);
+  if (!expr_res.is_ok) {
+    return (struct ParseFnResult){.is_ok = false,
+                                  .as.stmt = {0},
+                                  .msg = "Failed to parse expr after 'return'"};
+  }
+
+  struct Expr expr = expr_res.as.expr;
+
+  token_semicolon = consume(parser, TOKEN_SEMICOLON);
+  if (!token_semicolon) {
+    return (struct ParseFnResult){
+        .is_ok = false,
+        .as.stmt = {0},
+        .msg = "Expected token semicolon at the end of expr after 'return'"};
+  }
+
+  struct StmtRet ret_stmt;
+  ret_stmt.val = expr;
+
+  struct Stmt s;
+  s.kind = STMT_RET;
+  s.as.ret = ret_stmt;
+
+  result.as.stmt = s;
+
+  return result;
+}
+
+struct ParseFnResult parse_stmt(struct Parser *parser)
+{
+  struct ParseFnResult result;
+
+  result.is_ok = true;
+  result.msg = NULL;
+
+  switch (parser->curr->kind) {
+    case TOKEN_FN: {
+      struct ParseFnResult fn_res = parse_fn_stmt(parser);
+      if (!fn_res.is_ok) {
+        return (struct ParseFnResult){
+            .is_ok = false, .msg = "Failed parsing 'fn' stmt", .as.stmt = {0}};
+      }
+      result.as.stmt = fn_res.as.stmt;
+      break;
+    }
+    case TOKEN_RETURN: {
+      struct ParseFnResult ret_res = parse_ret_stmt(parser);
+      if (!ret_res.is_ok) {
+        return (struct ParseFnResult){
+            .is_ok = false, .msg = "Failed parsing 'ret' stmt", .as.stmt = {0}};
+      }
+      result.as.stmt = ret_res.as.stmt;
+      break;
+    }
+    default:
+      assert(0);
+  }
+
+  return result;
+}
+
+struct ParseResult parse(struct Parser *parser)
+{
+  struct Token *curr;
+  struct ParseResult result;
+
+  result.is_ok = true;
+  result.msg = NULL;
+
+  result.ast = malloc(sizeof(VecStmt));
+  result.ast->stmts = (VecStmt){0};
+
+  while (parser->curr) {
+    struct ParseFnResult r;
+
+    r = parse_stmt(parser);
+    if (!r.is_ok) {
+      return (struct ParseResult){.is_ok = false, .msg = r.msg, .ast = NULL};
+    }
+    vec_insert(&result.ast->stmts, r.as.stmt);
+  }
+
+  return result;
+}
+
+void print_expr(struct Expr *expr)
+{
+  switch (expr->kind) {
+    case EXPR_LITERAL: {
+      printf("Literal(%d)", expr->as.literal.as.num);
+      break;
+    }
+    case EXPR_BINARY: {
+      printf("Binary(lhs = ");
+      print_expr(expr->as.binary.lhs);
+      printf(", rhs = ");
+      print_expr(expr->as.binary.rhs);
+
+      switch (expr->as.binary.kind) {
+        case EXPR_BIN_ADD: {
+          printf(", kind = ADD)");
+          break;
+        }
+        case EXPR_BIN_SUB: {
+          printf(", kind = SUB)");
+          break;
+        }
+        case EXPR_BIN_MUL: {
+          printf(", kind = MUL)");
+          break;
+        }
+        case EXPR_BIN_DIV: {
+          printf(", kind = DIV)");
+          break;
+        }
+      }
+    }
+  }
+}
+
+void print_stmt(struct Stmt *stmt)
+{
+  switch (stmt->kind) {
+    case STMT_FN: {
+      printf("STMT_FN(name = %s, ", stmt->as.fn.name);
+      for (int i = 0; i < stmt->as.fn.body.len; i++) {
+        print_stmt(&stmt->as.fn.body.data[i]);
+      }
+      break;
+    }
+    case STMT_BLOCK: {
+      for (int i = 0; i < stmt->as.block.stmts.len; i++) {
+        print_stmt(&stmt->as.block.stmts.data[i]);
+      }
+      break;
+    }
+    case STMT_RET: {
+      printf("STMT_RET(val = ");
+      print_expr(&stmt->as.ret.val);
+      printf("\n");
+      break;
+    }
+    default:
+      assert(0);
+  }
+}
+
+void print_ast(struct AST *ast)
+{
+  for (int i = 0; i < ast->stmts.len; i++) {
+    print_stmt(&ast->stmts.data[i]);
+  }
+}
+
+void free_ast(struct AST *ast)
+{
+  return;
+}
+
 int main(void)
 {
   const char *path;
@@ -368,6 +948,9 @@ int main(void)
   struct Tokenizer tokenizer;
   struct TokenizeResult tokenize_result;
   VecToken tokens;
+  struct Parser parser;
+  struct ParseResult parse_result;
+  struct AST *ast;
 
   path = "spam.x";
 
@@ -389,6 +972,20 @@ int main(void)
 
   tokens = tokenize_result.tokens;
   print_tokens(tokens);
+
+  init_parser(&parser, &tokens);
+
+  parse_result = parse(&parser);
+  if (!parse_result.is_ok) {
+    fprintf(stderr, "err: %s\n", parse_result.msg);
+    goto free_up2_parse;
+  }
+
+  ast = parse_result.ast;
+  print_ast(ast);
+
+free_up2_parse:
+  free_ast(ast);
 
 free_up2_tokenize:
   vec_free(&tokenize_result.tokens);
